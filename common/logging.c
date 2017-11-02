7396ddc3e0b0c8a7b6db1221514b33fd5eb03a1a
edonyM <edonyzpc@gmail.com>
2017-11-02 22:53:09 +0800

    finish default logging method

--- common/logging.c
+++ common/logging.c
@@ -23,11 +23,11 @@
  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
 /* LOG LEVEL
  * Level    Numeric value
- * CRITICAL 50
- * ERROR    40
- * WARNING  30
- * INFO     20
- * DEBUG    10
+ * CRITICAL 5
+ * ERROR    4
+ * WARNING  3
+ * INFO     2
+ * DEBUG    1
  * NOTSET   0
  */
 
@@ -46,72 +46,157 @@
 #ifndef __FILENAME__
 #define __FILENAME__ (strrchr(__FILE__, '/') ? strrchr(__FILE__, '/') + 1 : __FILE__)
 #endif
+#define _PRINT_(f, format, ...) fprintf(f, format, ##__VA_ARGS__)
+#define _PRINT_C(format, ...) _PRINT_(stdout, format, ##__VA_ARGS__)
+#define _PRINT_E(format, ...) _PRINT_(stderr, format, ##__VA_ARGS__)
+#define _PRINT_F(format, ...) _PRINT_(LOGFILE, format, ##__VA_ARGS__)
 
-#define PRINTFILE(format, ...) \
-        fprintf(stdout, "[%s %s] %s(line %d, %s): " format, \
-                __DATE__, __TIME__, __FILENAME__, \
-                __LINE__, __FUNCTION__, ##__VA_ARGS__)
-
-#define DEBUG(func, format, ...) \
+/* default logging */
+// log into stdout
+#define _FMT_C(LEVEL, line, func) \
         time(&CURTIME); \
-        struct tm *lcl_tm=localtime(&CURTIME); \
-        fprintf(stdout, "[%d-%d-%d %d:%d:%d] <INFO> %s(line %d, %s): " format, \
-                lcl_tm->tm_year+1900, lcl_tm->tm_mon, lcl_tm->tm_mday, \
-                lcl_tm->tm_hour, lcl_tm->tm_min,lcl_tm->tm_sec, \
-                __FILENAME__, __LINE__, func, ##__VA_ARGS__)
+        CURTIME_tm=localtime(&CURTIME); \
+        _PRINT_C("[%d-%d-%d %d:%d:%d] <%s> %s(line %d, %s): ", \
+                CURTIME_tm->tm_year+1900, CURTIME_tm->tm_mon, CURTIME_tm->tm_mday, \
+                CURTIME_tm->tm_hour, CURTIME_tm->tm_min, CURTIME_tm->tm_sec, \
+                LEVEL, __FILENAME__, line, func)
 
-#define INFO(format, ...) \
-        fprintf(stdout, "[%s %s] <INFO> %s(line %d, %s): " format, \
-                __DATE__, __TIME__, __FILENAME__, \
-                __LINE__, __FUNCTION__, ##__VA_ARGS__)
+#define FMT_C(format, ...) _PRINT_C(format, ##__VA_ARGS__)
 
-#define WARNING(format, ...) \
-        fprintf(stdout, "[%s %s] <WARNING> %s(line %d, %s): " format, \
-                __DATE__, __TIME__, __FILENAME__, \
-                __LINE__, __FUNCTION__, ##__VA_ARGS__)
+// log into file
+#define _FMT_F(LEVEL, line, func) \
+        time(&CURTIME); \
+        CURTIME_tm=localtime(&CURTIME); \
+        _PRINT_F("[%d-%d-%d %d:%d:%d] <%s> %s(line %d, %s): ", \
+                CURTIME_tm->tm_year+1900, CURTIME_tm->tm_mon, CURTIME_tm->tm_mday, \
+                CURTIME_tm->tm_hour, CURTIME_tm->tm_min, CURTIME_tm->tm_sec, \
+                LEVEL, __FILENAME__, line, func)
 
-#define ERROR(format, ...) \
-        fprintf(stdout, "[%s %s] <ERROR> %s(line %d, %s): " format, \
-                __DATE__, __TIME__, __FILENAME__, \
-                __LINE__, __FUNCTION__, ##__VA_ARGS__)
+#define FMT_F(format, ...) _PRINT_F(format, ##__VA_ARGS__)
 
-#define CRITICAL(format, ...) \
-        fprintf(stdout, "[%s %s] <ERROR> %s(line %d, %s): " format, \
-                __DATE__, __TIME__, __FILENAME__, \
-                __LINE__, __FUNCTION__, ##__VA_ARGS__)
-/*
-#define ERR(format, ...) fprintf(stderr, "SRC_LINE=%d " format, __LINE__, ##__VA_ARGS__)
-*/
+// default logging
+#define _LOG_C(level, line, func, format, ...) \
+        _FMT_C(level, line, func); \
+        _PRINT_C(format, ##__VA_ARGS__)
 
-void print_info(int level, const char *func, const char *fmt, ...)
+#define _LOG_F(level, line, func, format, ...) \
+        _FMT_F(level, line, func); \
+        _PRINT_F(format, ##__VA_ARGS__)
+
+void print_c(int level, int line, const char *func, const char *fmt, ...)
 {
-	if (level == DEBUG)
-	{
-		char aMsgBuffer[MAX_LOG_LEN]={0};
-		va_list argptr;
+    char aMsgBuffer[MAX_LOG_LEN]={0};
+    va_list argptr;
 
-		va_start (argptr, fmt);
-		(void)vsnprintf(aMsgBuffer, sizeof(aMsgBuffer) - 1, fmt, argptr);
-		va_end (argptr);
-		aMsgBuffer[sizeof(aMsgBuffer)-1] = '\0';
-		DEBUG(func, "%s", aMsgBuffer);
-	}
+    va_start (argptr, fmt);
+    (void)vsnprintf(aMsgBuffer, sizeof(aMsgBuffer) - 1, fmt, argptr);
+    va_end (argptr);
+    aMsgBuffer[sizeof(aMsgBuffer)-1] = '\0';
+
+    char L[9] = {0};
+    switch (level) {
+        case NOTSET:
+            strcpy(L, "ALL");
+            //_LOG_C("ALL", line, func, "%s", aMsgBuffer);
+            break;
+        case DEBUG:
+            strcpy(L, "DEBUG");
+            //_LOG_C("DEBUG", line, func, "%s", aMsgBuffer);
+            break;
+        case INFO:
+            strcpy(L, "INFO");
+            //_LOG_C("INFO", line, func, "%s", aMsgBuffer);
+            break;
+        case WARNING:
+            strcpy(L, "WARNING");
+            //_LOG_C("WARNING", line, func, "%s", aMsgBuffer);
+            break;
+        case ERROR:
+            strcpy(L, "ERROR");
+            //_LOG_C("ERROR", line, func, "%s", aMsgBuffer);
+            break;
+        case CRITICAL:
+            strcpy(L, "CRITICAL");
+            //_LOG_C("CRITICAL", line, func, "%s", aMsgBuffer);
+            break;
+        default:
+            strcpy(L, "ALL");
+            _PRINT_C("Log Level Not Set And Log All\n");
+    }
+    _LOG_C(L, line, func, "%s", aMsgBuffer);
 }
 
-#define logging(level, fmt, ...) (print_info(level, __FUNCTION__, fmt, ##__VA_ARGS__))
+void print_f(const char *filename, int level, int line, const char *func, const char *fmt, ...)
+{
+    if (NULL == filename)
+        return;
+    if (!LOGFILE)
+        LOGFILE = fopen(filename, "a+");
+    char aMsgBuffer[MAX_LOG_LEN]={0};
+    va_list argptr;
+
+    va_start (argptr, fmt);
+    (void)vsnprintf(aMsgBuffer, sizeof(aMsgBuffer) - 1, fmt, argptr);
+    va_end (argptr);
+    aMsgBuffer[sizeof(aMsgBuffer)-1] = '\0';
+
+    char L[9] = {0};
+    switch (level) {
+        case NOTSET:
+            strcpy(L, "ALL");
+            //_LOG_C("ALL", line, func, "%s", aMsgBuffer);
+            break;
+        case DEBUG:
+            strcpy(L, "DEBUG");
+            //_LOG_C("DEBUG", line, func, "%s", aMsgBuffer);
+            break;
+        case INFO:
+            strcpy(L, "INFO");
+            //_LOG_C("INFO", line, func, "%s", aMsgBuffer);
+            break;
+        case WARNING:
+            strcpy(L, "WARNING");
+            //_LOG_C("WARNING", line, func, "%s", aMsgBuffer);
+            break;
+        case ERROR:
+            strcpy(L, "ERROR");
+            //_LOG_C("ERROR", line, func, "%s", aMsgBuffer);
+            break;
+        case CRITICAL:
+            strcpy(L, "CRITICAL");
+            //_LOG_C("CRITICAL", line, func, "%s", aMsgBuffer);
+            break;
+        default:
+            strcpy(L, "ALL");
+            _PRINT_C("Log Level Not Set And Log All\n");
+    }
+    _LOG_F(L, line, func, "%s", aMsgBuffer);
+}
+
+#define log(fmt, ...) print_c(NOTSET, __LINE__, __FUNCTION__, fmt, ##__VA_ARGS__)
+#define debug(fmt, ...) print_c(DEBUG, __LINE__, __FUNCTION__, fmt, ##__VA_ARGS__)
+#define info(fmt, ...) print_c(INFO, __LINE__, __FUNCTION__, fmt, ##__VA_ARGS__)
+#define warning(fmt, ...) print_c(WARNING, __LINE__, __FUNCTION__, fmt, ##__VA_ARGS__)
+#define error(fmt, ...) print_c(ERROR, __LINE__, __FUNCTION__, fmt, ##__VA_ARGS__)
+#define critical(fmt, ...) print_c(CRITICAL, __LINE__, __FUNCTION__, fmt, ##__VA_ARGS__)
+
+#define flog(filename, fmt, ...) print_f(filename, NOTSET, __LINE__, __FUNCTION__, fmt, ##__VA_ARGS__)
+#define fdebug(filename, fmt, ...) print_f(filename, DEBUG, __LINE__, __FUNCTION__, fmt, ##__VA_ARGS__)
+#define finfo(filename, fmt, ...) print_f(filename, INFO, __LINE__, __FUNCTION__, fmt, ##__VA_ARGS__)
+#define fwarning(filename, fmt, ...) print_f(filename, WARNING, __LINE__, __FUNCTION__, fmt, ##__VA_ARGS__)
+#define ferror(filename, fmt, ...) print_f(filename, ERROR, __LINE__, __FUNCTION__, fmt, ##__VA_ARGS__)
+#define fcritical(filename, fmt, ...) print_f(filename, CRITICAL, __LINE__, __FUNCTION__, fmt, ##__VA_ARGS__)
+
 
 void test(int d) {
-    print_info(DEBUG, __FUNCTION__, "I'm OK, %d\n", d);
-    logging(DEBUG, "I'm test, %d\n", d+111);
+    debug("I'm test, %d\n", d+111);
 }
 
-int main() {
-    INFO("I'm %d\n", 12);
-    int level = DEBUG;
 
-    sleep(10);
-    print_info(level, __FUNCTION__, "I'm edony %d\n", 111);
+int main() {
     test(128);
     sleep(5);
-    logging(level, "EDONY is %d\n", 1024);
+    info("EDONY is %d\n", 1024);
+    char *f = "log.txt";
+    finfo(f, "writing to file %p\n", f);
 }
--- include/logging.h
+++ include/logging.h
@@ -1,6 +1,7 @@
 #ifndef _LOGGING_H
 #define _LOGGING_H
 
+#include <stdio.h>
 #include <time.h>
 
 /*
@@ -12,8 +13,20 @@
  * DEBUG    10
  * NOTSET   0
  */
-enum LEVEL{NOTSET, DEBUG=10, INFO=20, WARNING=30, ERROR=40, CRITICAL=50};
+enum LEVEL{NOTSET=0, DEBUG, INFO, WARNING, ERROR, CRITICAL};
 
 time_t CURTIME;
+struct tm *CURTIME_tm;
 
+FILE *LOGFILE = NULL;
+
+struct log_cfg {
+    unsigned int level;     // log level(e.g. enum LEVEL)
+    unsigned int log2file;  // log into file(0 for no, 1 for yes)
+    const char *filename;   // if log2file flag set, filename is needed
+}LOGCFG={INFO, 0, NULL};
+void set_level(unsigned int level, struct log_cfg *LOGCFG);
+unsigned int get_level(struct log_cfg *LOGCFG);
+void set_logfile(char *filename);
+const char* get_logfile(struct log_cfg *logcfg);
 #endif
